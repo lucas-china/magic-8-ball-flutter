3853449031c83f8de83f3912da6788638c51defb
Lucas <lucasbrito_9@hotmail.com>
2020-04-15 22:08:59 -0300

    Added image in center of the screen.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,30 @@ void main() => runApp(
         title: Text('Ask Me Anything'),
         backgroundColor: Colors.blue[700],
       ),
+      body: MagicEightBall(),
     ),
   ),
 );
+
+class MagicEightBall extends StatefulWidget {
+  @override
+  _MagicEightBallState createState() => _MagicEightBallState();
+}
+
+class _MagicEightBallState extends State<MagicEightBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: null, 
+              child: Image.asset('images/ball1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
